ee42d56ec6f81f1afde6fea3982becf347b5c53a
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2014-09-07 01:16:54 +0300

    Prettify bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 PROJECTS_DIR="${HOME}/Projects"
-
 PROJECT_NAME="ansible-osx-developer"
 PROJECT_DIR="${PROJECTS_DIR}/${PROJECT_NAME}"
 
@@ -12,9 +11,9 @@ brew update && brew upgrade;
 
 brew install ansible;
 
-mkdir "${PROJECTS_DIR}";
+mkdir "${PROJECTS_DIR}" && cd "${PROJECT_DIR}";
+
 git clone https://github.com/pavel-v-chernykh/ansible-osx-developer.git "${PROJECT_DIR}";
-cd "${PROJECT_DIR}";
 
 ansible-playbook ./developer.yaml;
 

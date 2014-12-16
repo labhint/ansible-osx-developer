0b028558bb4e78bab9893a39749a041e8a30cb9a
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2014-12-16 14:27:37 +0200

    Update brew install link

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 PROJECT_DIR=`mktemp -d "/tmp/ansible-osx-developer.XXXXXXXXXX"`
 
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)";
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew doctor;
 brew update && brew upgrade;
